20ce4f47158d78066f86fb4a43af57643176cca1
f1p <f1rstperson@gmx.net>
2019-12-28 12:37:35 +0100

    mainly autostart and .profile

--- setup/de/all.sh
+++ setup/de/all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo apt-get install -y lightdm openbox \
-	xfce4 xfce4-whiskermenu-plugin xfce4-terminal \
+	xfce4 xfce4-whiskermenu-plugin xfce4-terminal caja \
 	dmenu nitrogen
 
 
-sudo apt-get purge -y xiccd xfdesktop4
+# get rid of some xfce components that i do not need
+sudo apt-get purge -y xiccd xfdesktop4 xfwm4 xfce4-panel
