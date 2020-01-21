08b81ea1acb3d2c2bc5fdeb2c8f9d69f331f9b36
Dominik Stahmer <dstahmer@local.recom.lan>
2020-01-21 17:53:35 +0100

    git gutter and markdown outline

--- stow/emacs/.emacs.d/config/languages/config-language-markdown.el
+++ stow/emacs/.emacs.d/config/languages/config-language-markdown.el
@@ -1,11 +1,22 @@
 (require 'config-programming-general)
 
-(config-add-external-dependency 'markdown 'config-language-markdown "previewing and exporting"
-                                (lambda () (executable-find "markdown"))
-                                "sudo apt-get install -y markdown" "None")
+;; (config-add-external-dependency 'markdown 'config-language-markdown "previewing and exporting"
+;;                                 (lambda () (executable-find "markdown"))
+;;                                 "sudo apt-get install -y markdown" "None")
 
 (use-package markdown-mode :ensure t :defer t
   :config
+
+  (defun fp/markdown-mode-hook ()
+    (interactive)
+    (outline-minor-mode 1))
+  (add-hook 'markdown-mode-hook 'fp/markdown-mode-hook)
+
+  (evil-define-key 'normal markdown-mode-map
+    (kbd "<tab>") 'markdown-cycle
+    "gj" 'markdown-outline-next-same-level
+    "gk" 'markdown-outline-previous-same-level)
+
   (use-package gh-md :ensure t
     :config
     (evil-leader/set-key-for-mode 'markdown-mode
