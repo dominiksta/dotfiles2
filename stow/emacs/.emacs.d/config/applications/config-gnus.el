221defd36ad62d6c3b1be3684804ee6c01623394
f1p <firstperson@gmx.net>
2020-03-31 20:31:04 +0200

    more gnus

--- stow/emacs/.emacs.d/config/applications/config-gnus.el
+++ stow/emacs/.emacs.d/config/applications/config-gnus.el
@@ -1,3 +1,5 @@
+(require 'gnus-group)
+
 ;; ----------------------------------------------------------------------
 ;; accounts
 ;; ----------------------------------------------------------------------
@@ -15,6 +17,24 @@
       gnus-asynchronous t
       gnus-agent t)
 
+;; Don't save newsrc; I don't care about compatibility with other news readers
+(setq gnus-read-newsrc-file nil
+      gnus-save-newsrc-file nil)
+
+;; Move gnus startup file out of $HOME
+(setq gnus-startup-file "~/.emacs.d/newsrc")
+
+;; ----------------------------------------------------------------------
+;; caching
+;; ----------------------------------------------------------------------
+
+;; Enable caching (reading offline). Use `gnus-summary-tick-article-forward' in
+;; a summary buffer to save specific articles. You can set
+;; `gnus-cacheable-groups' to a regexp to limit what groups can be cached.
+(setq gnus-use-cache t
+      gnus-cache-enter-articles '(read unread ticked dormant)
+      gnus-cache-remove-articles nil)
+
 ;; ----------------------------------------------------------------------
 ;; group buffer
 ;; ----------------------------------------------------------------------
@@ -27,6 +47,9 @@
       '(gnus-group-sort-by-unread
         gnus-group-sort-by-alphabet
         gnus-group-sort-by-rank))
+
+(define-key gnus-group-mode-map (kbd "q")
+  (lambda () (interactive) (gnus-save-newsrc-file) (quit-window)))
 
 ;; ----------------------------------------------------------------------
 ;; summary buffer
@@ -71,6 +94,8 @@
 (setq gnus-sum-thread-tree-vertical "│")
 
 (add-hook 'gnus-summary-mode-hook 'hl-line-mode)
+
+(add-hook 'gnus-summary-prepared-hook (lambda () (end-of-buffer) (previous-line)))
 
 ;; ----------------------------------------------------------------------
 ;; message buffer
