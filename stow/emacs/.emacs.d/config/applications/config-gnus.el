cf354f396efedf547bb0b13b589fb16be87f40c8
f1p <firstperson@gmx.net>
2020-04-02 00:17:47 +0200

    gnus cleanup and evil binds

--- stow/emacs/.emacs.d/config/applications/config-gnus.el
+++ stow/emacs/.emacs.d/config/applications/config-gnus.el
@@ -4,6 +4,8 @@
 ;; accounts
 ;; ----------------------------------------------------------------------
 
+;; For obvious privacy reasons, I do not want to have my e-mail accounts
+;; specified in my public dotfiles.
 (if (file-exists-p "~/sync/emacs/mail/gnus-accounts.el")
     (load "~/sync/emacs/mail/gnus-accounts.el")
   (warn (concat "No gnus-accounts file found in ~/sync/emacs/mail/gnus-accounts.el. "
@@ -13,56 +15,73 @@
 ;; general behaviour
 ;; ----------------------------------------------------------------------
 
-(setq gnus-directory "~/.emacs.d/News"
-      gnus-gcc-mark-as-read t
-      gnus-asynchronous t
-      gnus-agent t)
+(setq
+ ;; I don't like my $HOME getting messed up more than it needs to be.
+ gnus-directory "~/.emacs.d/News"
+ gnus-startup-file "~/.emacs.d/newsrc"
+ ;; The gcc header can be used to put e-mail into other folders upon being
+ ;; sent. Obviously I don't want e-mails in "Sent" to be unread by default.
+ gnus-gcc-mark-as-read t
+ ;; I'm not 100% sure what this does. But apparently, it enables gnus to
+ ;; fetch articles in advance some of the time.
+ gnus-asynchronous t)
 
 ;; Don't save newsrc; I don't care about compatibility with other news readers
 (setq gnus-read-newsrc-file nil
       gnus-save-newsrc-file nil)
 
-;; Move gnus startup file out of $HOME
-(setq gnus-startup-file "~/.emacs.d/newsrc")
-
 ;; ----------------------------------------------------------------------
 ;; caching
 ;; ----------------------------------------------------------------------
 
-;; Enable caching (reading offline). Use `gnus-summary-tick-article-forward' in
-;; a summary buffer to save specific articles.
+;; Gnus has two different ways of doing caching. The "regular" cache, enabled by
+;; `gnus-use-cache' is mostly intended for manual use. So if you want to
+;; explicitly save an article to disk, you would use that. This cache stores
+;; things based on marks. What marks enter the cache is determined by
+;; `gnus-cache-enter-articles' and what exits the cache by
+;; `gnus-cache-remove-articles'. So, by default you can use
+;; `gnus-summary-tick-article-forward' to manually cache something.
 (setq gnus-use-cache t)
 
-;; Add all opened articles to the agent cache
-(add-hook 'gnus-select-article-hook 'gnus-agent-fetch-selected-article)
+;; The gnus "agent" is the other, more modern and automated way of doing
+;; caching. It is supposed to run mostly invisibly without user interaction.
+(setq gnus-agent t)
 
+;; To actually make it work as expected and enter all articles you fetch
+;; automatically to it's cache, you use this incantation:
+(add-hook 'gnus-select-article-hook 'gnus-agent-fetch-selected-article)
 
 ;; ----------------------------------------------------------------------
 ;; group buffer
 ;; ----------------------------------------------------------------------
 
-(add-hook 'gnus-group-mode-hook 'hl-line-mode)
+;; To categorize groups, we can use "topics". All topic actions in the group
+;; buffer start with the prefix "T". Personally, I use topics create a
+;; traditional e-mail client interface where I have the folders of my different
+;; e-mail accounts grouped together by account. The alignment of these groups
+;; gets saved to your newsrc.
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
-;; Hit C-c C-s to apply this sorting
+;; I like to sort by rank first, alphabet second. You can hit C-c C-s to apply
+;; this sorting.
 (setq gnus-group-sort-function
       '(gnus-group-sort-by-unread
         gnus-group-sort-by-alphabet
         gnus-group-sort-by-rank))
 
+;; I prefer gnus to keep running in the background, so i just tell it so save my
+;; newsrc and hide the buffer instead of exiting.
 (define-key gnus-group-mode-map (kbd "q")
   (lambda () (interactive) (gnus-save-newsrc-file) (quit-window)))
+
+;; Visuals
+(add-hook 'gnus-group-mode-hook 'hl-line-mode)
 
 ;; ----------------------------------------------------------------------
 ;; summary buffer
 ;; ----------------------------------------------------------------------
 
 ;; --- formatting of entries ---
-(setq gnus-user-date-format-alist
-      '(((gnus-seconds-today) . "Today, %H:%M")
-        ((+ 86400 (gnus-seconds-today)) . "Yesterday, %H:%M")
-        (t . "%Y-%m-%d %H:%M")))
-
 (setq gnus-summary-line-format
       (concat
        "%R%U%uR%z"         ; cached, read, `gnus-user-format-function-R' , score
@@ -76,17 +95,10 @@
        "\n"                ; end
        ))
 
-(defun gnus-user-format-function-R (header)
-  "I display forwarded and replied to myself instead of using %R
-because the default %R prints the \"cached\" mark with a higher
-precedence than forwarded or replied to, which i don't like since
-i mostly cache everything."
-  (let* ((id (gnus-id-to-article (mail-header-id header)))
-         (replied (memq id gnus-newsgroup-replied))
-         (forwarded (memq id gnus-newsgroup-forwarded)))
-    (cond (replied "A")
-          (forwarded "F")
-          (t " "))))
+(setq gnus-user-date-format-alist
+      '(((gnus-seconds-today) . "Today, %H:%M")
+        ((+ 86400 (gnus-seconds-today)) . "Yesterday, %H:%M")
+        (t . "%Y-%m-%d %H:%M")))
 
 ;; 32 the char for a space
 (setq gnus-replied-mark 32
@@ -125,13 +137,143 @@ i mostly cache everything."
 ;; dired interaction
 ;; ----------------------------------------------------------------------
 
+;; This mostly just allows you to attach files from dired to a message by
 (turn-on-gnus-dired-mode)
 
 ;; ----------------------------------------------------------------------
 ;; sending mail
 ;; ----------------------------------------------------------------------
 
+(setq message-cite-style
+      '((message-cite-function 'message-cite-original)
+        (message-citation-line-function 'message-insert-formatted-citation-line)
+        (message-cite-reply-position 'above)
+        (message-yank-prefix "> ")
+        (message-yank-cited-prefix ">")
+        (message-yank-empty-prefix ">")
+        (message-citation-line-format "On %D %R %p, %N wrote:")))
+
 (add-to-list 'auto-mode-alist (cons "\\.message\\'" 'message-mode))
+
+
+;; ----------------------------------------------------------------------
+;; evil binds
+;; ----------------------------------------------------------------------
+
+
+;; ----------------------------------------------------------------------
+;; summary
+;; ----------------------------------------------------------------------
+(evil-set-initial-state 'gnus-summary-mode 'normal)
+(evil-define-key 'normal gnus-summary-mode-map
+  ;; session
+  "q" 'gnus-summary-exit
+  "Q" 'gnus-summary-exit-no-update
+  "r" 'gnus-summary-rescan-group
+
+  ;; writing mail
+  (kbd "M-r") 'gnus-summary-reply
+  (kbd "M-R") 'gnus-summary-reply-with-original
+  (kbd "M-f") 'gnus-summary-mail-forward
+  (kbd "M-m") 'gnus-summary-mail-other-window
+
+  ;; movement
+  "J" 'gnus-summary-next-article
+  "K" 'gnus-summary-prev-article
+  (kbd "RET") 'gnus-summary-scroll-up
+  "ga" 'gnus-summary-goto-article
+  "zz" 'gnus-recenter
+
+  ;; marking and executing
+  "m" 'gnus-summary-mark-as-processable
+  "u" 'gnus-summary-unmark-as-processable
+  "U" 'gnus-summary-unmark-all-processable
+  "x" 'gnus-summary-universal-argument
+
+  "o" 'gnus-summary-put-mark-as-read-next
+  "O" 'gnus-summary-put-mark-as-unread-next
+
+  "b" 'gnus-summary-move-article
+
+  "zt" 'gnus-summary-toggle-header
+
+  ;; threads
+  "tt" 'gnus-summary-toggle-threads
+  "tf" 'gnus-summary-refer-thread
+
+  ;; sorting
+  "sa" 'gnus-summary-sort-by-author
+  "sc" 'gnus-summary-sort-by-chars
+  "sd" 'gnus-summary-sort-by-date
+  "si" 'gnus-summary-sort-by-score
+  "sl" 'gnus-summary-sort-by-lines
+  "smd" 'gnus-summary-sort-by-most-recent-date
+  "smm" 'gnus-summary-sort-by-marks
+  "smn" 'gnus-summary-sort-by-most-recent-number
+  "sn" 'gnus-summary-sort-by-number
+  "so" 'gnus-summary-sort-by-original
+  "sr" 'gnus-summary-sort-by-random
+  "ss" 'gnus-summary-sort-by-subject
+  "st" 'gnus-summary-sort-by-recipient)
+
+;; ----------------------------------------------------------------------
+;; article
+;; ----------------------------------------------------------------------
+(evil-set-initial-state 'gnus-article-mode 'normal)
+(evil-define-key 'normal gnus-article-mode-map
+  (kbd "M-r") 'gnus-summary-reply
+  (kbd "M-R") 'gnus-summary-reply-with-original
+  "q" 'evil-window-delete)
+
+;; ----------------------------------------------------------------------
+;; group
+;; ----------------------------------------------------------------------
+(evil-set-initial-state 'gnus-group-mode 'normal)
+(evil-define-key 'normal gnus-group-mode-map
+  "q" (lambda () (interactive) (gnus-save-newsrc-file) (quit-window))
+
+  "x" 'gnus-group-kill-group
+  "p" 'gnus-group-yank-group
+
+  "r" 'gnus-group-get-new-news
+  "R" (lambda () (interactive) (gnus-group-get-new-news '(4)))
+
+  "T" 'gnus-group-topic-map
+
+  "gs" 'gnus-group-enter-server-mode
+
+  (kbd "RET") 'gnus-group-select-group
+
+  "M-m" 'gnus-group-mail)
+
+;; ----------------------------------------------------------------------
+;; server
+;; ----------------------------------------------------------------------
+(evil-set-initial-state 'gnus-server-mode 'normal)
+(evil-define-key 'normal gnus-server-mode-map
+  "q"         'gnus-server-exit
+  (kbd "RET") 'gnus-server-read-server
+
+  ;; online status
+  "C"         'gnus-server-close-server
+  "D"         'gnus-server-deny-server
+  "L"         'gnus-server-offline-server
+  "O"         'gnus-server-open-server
+
+  "r"         'gnus-server-regenerate-server
+  "x"         'gnus-server-kill-server
+  "s"         'gnus-server-scan-server
+  "p"         'gnus-server-yank-server
+  )
+
+;; ----------------------------------------------------------------------
+;; browse servers
+;; ----------------------------------------------------------------------
+(evil-set-initial-state 'gnus-browse-mode 'normal)
+(evil-define-key 'normal gnus-browse-mode-map
+  "q"         'gnus-browse-exit
+  "u"         'gnus-browse-unsubscribe-current-group
+  (kbd "RET") 'gnus-browse-read-group)
 
 
 (provide 'config-gnus)
