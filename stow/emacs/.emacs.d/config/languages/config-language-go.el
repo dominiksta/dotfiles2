1264b93a4be1c4dc6cefb2c973cb78e407968841
f1p <firstperson@gmx.net>
2020-04-08 22:48:20 +0200

    additional golang config

--- stow/emacs/.emacs.d/config/languages/config-language-go.el
+++ stow/emacs/.emacs.d/config/languages/config-language-go.el
@@ -1,5 +1,9 @@
 (require-and-log 'config-programming-general)
 
-(use-package go-mode :ensure t)
+(use-package go-mode :ensure t :config
+  (add-hook 'go-mode-hook
+            (lambda () (add-hook 'before-save-hook 'gofmt-before-save nil t)))
+  (evil-define-key 'normal go-mode "gd" 'godef-jump)
+  (evil-leader/set-key-for-mode 'go-mode "md" 'godoc-at-point))
 
 (provide 'config-language-go)
