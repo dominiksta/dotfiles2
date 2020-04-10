e6b2eb6b11ce9caa019829fbe3097f5f439abee5
f1p <firstperson@gmx.net>
2020-04-10 22:07:34 +0200

    java indentation

--- stow/emacs/.emacs.d/config/languages/config-language-java.el
+++ stow/emacs/.emacs.d/config/languages/config-language-java.el
@@ -12,6 +12,15 @@
 ;;   (add-hook 'java-mode-hook 'lsp)
 ;;   (evil-leader/set-key-for-mode 'java-mode "mo" 'lsp-java-organize-imports))
 
+(defun fp/java-indentation-setup ()
+  (setq-local c-default-style "java")
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'arglist-cont-nonempty '+)
+  (c-set-offset 'arglist-close '0)
+  (c-set-offset 'case-label '+))
+
+(add-hook 'java-mode-hook 'fp/java-indentation-setup)
+
 (provide 'config-language-java)
 
 
