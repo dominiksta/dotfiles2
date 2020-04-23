4b37cfc0ed4e82611d005a8e316cd2af5228b78e
f1p <firstperson@gmx.net>
2020-04-23 22:42:59 +0200

    rename tabbed-urxvtc to urxvtc-tabbed

---
+++ stow/urxvt/bin/urxvtc-tmux-new-window.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TMUX_SESSION_NAME="main"
+
+if ! tmux list-sessions | grep -q $TMUX_SESSION_NAME; then
+    tmux new-session -d -s $TMUX_SESSION_NAME -c "$PWD"
+else
+    tmux new-window -c "$PWD"
+fi 
+
+wmctrl -x -a urxvt || urxvtc -e tmux attach -t $TMUX_SESSION_NAME
