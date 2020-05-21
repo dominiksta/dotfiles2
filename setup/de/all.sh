16e90ff56d1934fce9de88fe166ac7340c9c8c1c
f1p <f1rstperson@gmx.net>
2020-05-21 15:46:24 +0200

    yaru cursor theme

--- setup/de/all.sh
+++ setup/de/all.sh
@@ -8,6 +8,6 @@ apt-get install -y stow \
         dmenu j4-dmenu-desktop gsimplecal tint2 plank \
         redshift xdotool wmctrl xclip \
         xkbset xcape sxhkd \
-	ttf-dejavu
+	ttf-dejavu yaru-theme-icon
 
 git config --global user.email "f1rstperson@gmx.net"
