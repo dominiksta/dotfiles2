60213ea939daeb4a735fbfe618bfb0b76ea0ae53
f1p <firstperson@gmx.net>
2020-09-22 14:05:00 +0200

    trying doom themes again

--- stow/emacs/.emacs.d/config/config-ui.el
+++ stow/emacs/.emacs.d/config/config-ui.el
@@ -91,20 +91,22 @@ fixed-pitch faces."
 ;;   (setq fp/theme-light-theme 'sanityinc-tomorrow-day
 ;;         fp/theme-dark-theme 'sanityinc-tomorrow-night))
 
-(use-package modus-operandi-theme :ensure t
-  :config
-  (setq fp/theme-light-theme 'modus-operandi))
-
-(use-package modus-vivendi-theme :ensure t
-  :config
-  (setq fp/theme-dark-theme 'modus-vivendi))
-
-;; (use-package doom-themes
-;;   :ensure t
+;; (use-package modus-operandi-theme :ensure t
 ;;   :config
-;;   (doom-themes-org-config)
-;;   (setq fp/theme-light-theme 'doom-one-light
-;;         fp/theme-dark-theme 'doom-city-lights))
+;;   (setq fp/theme-light-theme 'modus-operandi))
+
+;; (use-package modus-vivendi-theme :ensure t
+;;   :config
+;;   (setq fp/theme-dark-theme 'modus-vivendi))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (doom-themes-org-config)
+  (custom-set-faces
+   '(highlight ((t (:underline t :background nil :foreground nil :distant-foreground nil)))))
+  (setq fp/theme-light-theme 'doom-one-light
+        fp/theme-dark-theme 'doom-one))
 
 ;; (use-package "spacemacs-theme"
 ;;   :ensure t
