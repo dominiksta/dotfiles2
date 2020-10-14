db1362616b566a50f722fbf763e22722ee2112d6
f1p <firstperson@gmx.net>
2020-10-14 22:10:56 +0200

    new copy instead of symlink function for dots

--- stow/generic-scripts/bin/relink-dotfiles.sh
+++ stow/generic-scripts/bin/relink-dotfiles.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-pushd .
+pushd . > /dev/null
+
+DEBUG=false
+
+# ----------------------------------------------------------------------
+# copy
+# ----------------------------------------------------------------------
+
+cd ~/git/dotfiles/stow-copy/
+
+for group in */ ; do
+    cd $group
+    $DEBUG && echo "pwd: $(pwd)"
+
+    for file in $(find . -type f); do
+        dest=$(echo $file | sed "s/\./\$HOME/")
+        eval dest=$dest
+        $DEBUG && echo "running mkdir -p $(dirname $dest)"
+        mkdir -p "$(dirname $dest)"
+        $DEBUG && echo "running cp -f $file $dest"
+        cp -f $file $dest
+    done
+
+    cd ..
+done
+
+# ----------------------------------------------------------------------
+# stow
+# ----------------------------------------------------------------------
 
 mkdir -p ~/.config/autostart/
 mkdir -p ~/.emacs.d/
@@ -9,4 +37,4 @@ mkdir -p ~/bin/
 cd ~/git/dotfiles/stow
 stow -v --target=$HOME --restow *
 
-popd
+popd > /dev/null
