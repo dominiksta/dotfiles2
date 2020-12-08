53c589d15bebba06155ce682a3994991bf615e81
f1p <firstperson@gmx.net>
2020-12-08 20:30:03 +0100

    latex visuals, math and references

--- stow/emacs/.emacs.d/config/languages/config-language-latex.el
+++ stow/emacs/.emacs.d/config/languages/config-language-latex.el
@@ -6,27 +6,53 @@
 
 (use-package auctex :ensure t :defer t)
 
-(setq LaTeX-item-indent 0 ;; \item should be indented by 2 spaces
-      TeX-view-program-selection '((output-pdf "PDF Tools")))
+;; ----------------------------------------------------------------------
+;; Visuals
+;; ----------------------------------------------------------------------
+(add-hook 'TeX-mode-hook 'olivetti-mode)
+(add-hook 'TeX-mode-hook 'hl-todo-mode)
 
-(defun fp/tex-mode-hook ()
-  (TeX-source-correlate-mode 1) ;; `TeX-view' should jump to the location under
-  ;; point, not just the relevant file. (Uses synctex)
-  )
+;; ----------------------------------------------------------------------
+;; Random
+;; ----------------------------------------------------------------------
 
-(add-hook 'TeX-mode-hook 'fp/tex-mode-hook)
+(setq
+ ;; \item should be indented by 2 spaces
+ LaTeX-item-indent 0
+ ;; use pdf-tools as a viewer
+ TeX-view-program-selection '((output-pdf "PDF Tools")))
+
+(add-hook 'TeX-mode-hook 'TeX-source-correlate-mode)
 
 (evil-leader/set-key-for-mode 'latex-mode
   "ma" 'TeX-command-run-all
-  "mc" 'TeX-command-master)
+  "mC" 'TeX-command-master)
 
 (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
 (setq-default TeX-master nil)
 
-(evil-leader/set-key-for-mode 'bibtex-mode
-  "if" 'bibtex-fill-entry)
+;; ----------------------------------------------------------------------
+;; Math
+;; ----------------------------------------------------------------------
 
-(use-package helm-bibtex :ensure t)
+(add-hook 'latex-mode-hook 'prettify-symbols-mode)
+(evil-leader/set-key-for-mode 'latex-mode "mp" 'prettify-symbols-mode)
 
+;; ----------------------------------------------------------------------
+;; References
+;; ----------------------------------------------------------------------
+
+(setq bibtex-dialect 'biblatex
+      bibtex-completion-library-path (concat sync-directory "Documents/academic/")
+      reftex-default-bibliography (concat sync-directory "Documents/zotero/library.bib"))
+
+;; I use `zotero` with the `Better BibTeX` plugin for library management.
+(use-package helm-bibtex :ensure t :config
+  (setq bibtex-completion-bibliography
+        (list reftex-default-bibliography)
+        bibtex-completion-pdf-field "file"))
+
+;; If i need to edit bibtex entries manually, this has proven useful.
+(evil-leader/set-key-for-mode 'bibtex-mode "if" 'bibtex-fill-entry)
 
 (provide 'config-language-latex)
