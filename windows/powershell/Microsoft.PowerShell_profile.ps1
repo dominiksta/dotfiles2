5235cacc7e785bf48fd5366e8523146f1bbc9d1a
unknown <f1p@posteo.de>
2021-02-22 14:11:04 +0100

    add initial powershell profile and windows-terminal settings

---
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+# Will be automatically executed by powershell if saved as the filename in
+# `$profile`. By default, this is
+# `$env:Userprofile\Documents\WindowsPowerShell\`.
+
+# If I hand only known this was possible earlier.
+Set-PSReadLineOption -EditMode Emacs
