92a585808e792b430071dabddea823f50a1f096b
f1p <f1p@posteo.de>
2021-03-06 12:13:08 +0100

    vcvars in powershell

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,35 @@ Set-PSReadLineOption -EditMode Emacs
 
 
 function prompt {
-  $p = Split-Path -Leaf -Path (Get-Location)
-  "PS $p> "
+    $p = Split-Path -Leaf -Path (Get-Location)
+    "PS $env:my__loaded_environment$p> "
+}
+
+Set-Alias -Name em -Value emacsclientw.exe
+Set-Alias -Name gfind -Value "C:\Program Files\Git\usr\bin\find.exe"
+
+Function admin { nircmd.exe elevate $args }
+Function admincmd { nircmd.exe elevate cmd /k $args }
+Function adminpsh { nircmd.exe elevate powershell /k $args }
+
+Function Load-EnvFromBat {
+    param( [string]$BatFile )
+    $dir = (Split-Path -Path $BatFile)
+    $file = (Split-Path -Leaf $BatFile)
+    pushd "$dir"
+    echo "Loading $file ..."
+    cmd /c "$file&set" |
+      foreach {
+          if ($_ -match "=") {
+              $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+          }
+      }
+    popd
+    Write-Host "`n$file variables set." -ForegroundColor Yellow
+}
+
+Function vcvars2019x64 {
+    Load-EnvFromBat -BatFile "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+    $env:my__loaded_environment = "vs2019x64 "
+    Write-Host "Environment for 'x64 Native Tools Command Prompt for VS 2019'"
 }
