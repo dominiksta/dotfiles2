38d70b7bd7c020d319f4deed9b263e996a30561c
Dominik Stahmer <dominik.stahmer@posteo.de>
2021-10-16 16:45:15 +0200

    citing/saving webpages in mvtn

--- stow/emacs/.emacs.d/config/applications/config-mvtn.el
+++ stow/emacs/.emacs.d/config/applications/config-mvtn.el
@@ -3,6 +3,10 @@
 (straight-use-package '(mvtn :type git :host github :repo "dominiksta/mvtn.el"))
 
 (with-eval-after-load "mvtn"
+  ;; ----------------------------------------------------------------------
+  ;; general
+  ;; ----------------------------------------------------------------------
+
   (setq mvtn-note-directories
         '((:dir "~/sync/documents/notes/mvtn" :name "prv" :structure
                 ((:dir "flt" :datetree t) ;; fleeting
@@ -27,23 +31,65 @@
 
   (mvtn-journal-autojournal-set-feature 'git-commit t)
   (mvtn-journal-autojournal-set-feature 'note-changed t)
-  (mvtn-journal-autojournal-set-feature 'org-clock t))
+  (mvtn-journal-autojournal-set-feature 'org-clock t)
 
-(evil-define-key 'normal mvtn-tag-file-list-mode-map
-  "o" 'mvtn-tag-file-list-open
-  "a" 'mvtn-tag-file-list-open-keep-focus
-  "r" 'revert-buffer
-  "q" 'quit-window)
+  ;; ----------------------------------------------------------------------
+  ;; citing webpages
+  ;; ----------------------------------------------------------------------
 
-;; if `evil-org-mode' is not disabled, it will overshadow the binds in the
-;; following section
-(add-hook 'mvtn-backlink-buffer-mode-hook
-          (lambda () (interactive) (evil-org-mode 0)))
-(evil-define-key 'normal mvtn-backlink-buffer-mode-map
-  "gj" 'mvtn-backlink-buffer-next-backlink
-  (kbd "M-j") 'mvtn-backlink-buffer-next-backlink
-  "gk" 'mvtn-backlink-buffer-previous-backlink
-  (kbd "M-k") 'mvtn-backlink-buffer-previous-backlink)
+  (defun fp/mvtn-store-webpage-from-clipboard-markdown (encrypt)
+    (interactive "P")
+    "This uses the 'Copy Selection as Markdown'
+Addon (https://github.com/0x6b/copy-selection-as-markdown).
+Integration of the kill-ring with the system clipboard has to be
+enabled (it is by default)."
+    (let* ((clip (current-kill 0))
+           (first-line (car (split-string clip "\n")))
+           (the-rest (mapconcat 'identity (cddr (split-string clip "\n")) "\n")))
+      (save-match-data
+        (string-match "^\\[\\(.+\\)\\](\\(.+\\))" first-line)
+        (let ((title (match-string-no-properties 1 first-line))
+              (url (match-string-no-properties 2 first-line)))
+          (mvtn-new-note-from-template-string
+           "prv/flt" title "org" '("web")
+           (format "#+TITLE: {title}
+#+DATE: {date}
+# mvtn_original_title :: {title}
+# mvtn_original_id :: {timestamp}
+
+{point}
+
+Quelle: %s
+
+#+BEGIN_SRC markdown
+%s
+#+END_SRC\n" url the-rest))))))
+
+  ;; ----------------------------------------------------------------------
+  ;; binds
+  ;; ----------------------------------------------------------------------
+
+  (evil-define-key 'normal mvtn-tag-file-list-mode-map
+    "o" 'mvtn-tag-file-list-open
+    "a" 'mvtn-tag-file-list-open-keep-focus
+    "r" 'revert-buffer
+    "q" 'quit-window)
+
+  ;; If `evil-org-mode' is not disabled, it will overshadow the binds in the
+  ;; following section
+  (add-hook 'mvtn-backlink-buffer-mode-hook
+            (lambda () (interactive) (evil-org-mode 0)))
+  (evil-define-key 'normal mvtn-backlink-buffer-mode-map
+    "gj" 'mvtn-backlink-buffer-next-backlink
+    (kbd "M-j") 'mvtn-backlink-buffer-next-backlink
+    "gk" 'mvtn-backlink-buffer-previous-backlink
+    (kbd "M-k") 'mvtn-backlink-buffer-previous-backlink))
+
+
+;; This of course does not not /really/ come from mvtn.el but autoloading like
+;; this will trigger the `with-eval-after-load' block above which will define
+;; the function.
+(autoload 'fp/mvtn-store-webpage-from-clipboard-markdown "mvtn.el")
 
 (evil-leader/set-key
   "nd" 'mvtn-jump-current-year-directory
@@ -57,7 +103,8 @@
   "nb" 'mvtn-backlink-buffer-toggle-side-window
   "nl" 'mvtn-insert-link
   "no" 'mvtn-follow-link-at-point
-  "nt" 'mvtn-tag-file-list)
+  "nt" 'mvtn-tag-file-list
+  "nw" 'fp/mvtn-store-webpage-from-clipboard-markdown)
 
 
 (provide 'config-mvtn)
